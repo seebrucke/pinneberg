750e555309d02e5dbaa2cc8b644ff7b23a9e532d
mojtaba <mojtaba.zargar2010@gmail.com>
2018-11-04 16:48:03 +0100

    ttt#

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
-app.use('/' , (req,res) => {
-    res.send('hello world');
-});
-app.listen(3001,() => {
-    console.log('Listening on port 3000');
-});
+app.use('/' , express.static('index.html'));
+app.use('/about',express.static('about.html'));
+app.use('/contact',express.static('contact.html'));
+app.use('/german',express.static('german'));
+app.use('/arabi',express.static('arabi'));
+app.use('/farsi',express.static('farsi'));
+app.listen(3000,() => {console.log('listening....')});
